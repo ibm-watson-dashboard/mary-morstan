1ae821c374869a0469347bf21498973b1868c4f1
Leandro Vieira <leandro.vieira@softplan.com.br>
2019-07-21 18:08:48 -0300

    First commit

---
+++ src/main/java/br/com/lepsistemas/marymorstan/MaryMorstanApplication.java
@@ -0,0 +1,13 @@
+package br.com.lepsistemas.marymorstan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaryMorstanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaryMorstanApplication.class, args);
+	}
+
+}
